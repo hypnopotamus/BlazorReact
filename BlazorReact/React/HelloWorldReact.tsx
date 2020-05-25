24f98494e029b967bfcca37ce35cd0d224fc534c
christopher dykstra <chrdyks@gmail.com>
2020-05-25 13:06:53 -0500

    POC project setup

---
+++ BlazorReact/React/HelloWorldReact.tsx
@@ -0,0 +1,10 @@
+﻿import * as React from 'react';
+import ReactDOM from 'react-dom';
+
+const Component = () => <p>Hello from React!</p>;
+
+export default function hello(renderElement: HTMLElement) {
+    ReactDOM.render(<Component />, renderElement);
+}
+
+(window as any).renderReact = hello;
